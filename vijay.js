c0d4dc34aa97320dfbf4f6ecc2db312d9abc0c95
vijaySB <99825352+vijaykrishnaSB@users.noreply.github.com>
2022-05-20 01:33:37 +0530

    Update vijay.js

--- vijay.js
+++ vijay.js
@@ -7,19 +7,19 @@ for(item of buttons) {
         console.log("Button text is", buttonText);
         if(buttonText == "X"){
             buttonText = "*";
-            screen.Value += buttonText;
-            screen.Value = screenValue;
+            screenValue += buttonText;
+            screen.value = screenValue;
         }
         else if(buttonText == "C") {
             screenValue = "";
-            screenValue = screenValue;
+            screen.value = screenValue;
         }
         else if (buttonText == "="){
-            screen.Value =eval(screenValue);
+            screen.value = eval(screenValue);
         }
         else{
             screenValue += buttonText;
-            screen.Value = screenValue;
+            screen.value = screenValue;
         }
     });
 };
